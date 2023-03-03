bd344ca16032b47285fac5f9359bb6ea1fc190a4
Ajay Prajapati <prajapatisani9@gmail.com>
2023-03-03 23:28:49 +0530

    till video 11

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 
 function App() {
   return (
     <div className="App">
       <Greeting/>
+      <Async/>
     </div>
   );
 }
